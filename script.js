9b718c557b229c8d1953f88973a25476e34b5219
Shivani-18094 <98810907+Shivani-18094@users.noreply.github.com>
2023-12-27 14:41:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  if(scroll>=10){
+    $(".navbar").addClass("nav-scroll");
+    $(".back_to_top").addClass("show_btn");
+  } else{
+    $(".navbar").removeClass("nav-scroll");
+    $(".back_to_top").removeClass("show_btn");
+  }
+});
+
+
+$('.testimonial_img').slick({
+  dots: true,
+  arrows:false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+AOS.init();
